d989a875e5dcc06447fe2ab9e95f3d374fa660eb
Jesper Kristensen <jek@usekeyhole.com>
2024-04-10 14:22:46 +0200

    Checkeed efc

---
+++ Efc/DataContext.cs
@@ -0,0 +1,41 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Efc;
+
+public class DataContext : DbContext
+{
+    public DbSet<Drink> Drinks { get; set; }
+    public DbSet<DrinksMenu> DrinkMenus { get; set; }
+
+    public string DbPath { get; }
+
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // {
+    //     optionsBuilder.UseSqlite("Data Source = Data/DataBase.db");            
+    // }
+
+    /// <summary>
+    /// I ran in to some issues with the path to the database file.
+    /// while running it in a console application it went through the debug files, and the database coudn't be reached.
+    ///  in the string dbPath i used the .. to go out of the debug scope and in to the Data folder.
+    /// </summary>
+    /// <param name="optionsBuilder"></param>
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string dbFileName = "DataBase.db";
+        string dbPath = Path.Combine("..", "..", "..", "Data", dbFileName);
+        string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string fullDbPath = Path.GetFullPath(Path.Combine(currentDirectory, dbPath));
+
+        optionsBuilder.UseSqlite($"Data Source={fullDbPath}");
+        Console.WriteLine(fullDbPath);
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Drink>().HasKey(d => d.DrinkId);
+        modelBuilder.Entity<DrinksMenu>().HasKey(dm => dm.DrinkMenuId);
+    }
+}
---
+++ Efc/Entities/DrinksMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class DrinksMenu
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int DrinkMenuId { get; set; }
+
+    [Required] public string Name { get; set; }
+    [Required] public bool ContainsAlcohol { get; set; }
+    [Required] public DateTime AvailableFrom { get; set; }
+    public List<Drink> Drinks { get; set; }
+}
