be16e7e7d00fcb604d0797d950442187c4e1b959
pilsner91 <morten_oel@hotmail.com>
2024-04-17 10:59:08 +0200

    added remove function on blazor side

--- BlazorApp1/HttpClient/ItemHttpClient/IItemHttpClient.cs
+++ BlazorApp1/HttpClient/ItemHttpClient/IItemHttpClient.cs
@@ -6,4 +6,5 @@ namespace BlazorApp1.HttpClient.ItemHttpClient;
 public interface IItemHttpClient
 {
     public Task<Item> CreateItem (CreateItemDAO item);
+    public Task<Item> TakeItem(int ItemID);
 }
