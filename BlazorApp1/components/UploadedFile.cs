86606c7ad32a2ff61017cee1c232e917ca3bab28
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2024-05-20 11:40:25 +0200

    setting up the ai

---
+++ BlazorApp1/components/UploadedFile.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp1.components;
+
+public class UploadedFile
+{
+    public string Name { get; set; }
+    public byte[] Content { get; set; }
+    public string Preview { get; set; }
+}
