6a3ada45b6fb51e9b816e20711f8896bcdecd58d
Jesper Kristensen <jek@usekeyhole.com>
2024-05-20 23:32:47 +0200

    nb

--- BlazorApp1/components/UploadedFile.cs
+++ BlazorApp1/components/UploadedFile.cs
@@ -5,4 +5,5 @@ public class UploadedFile
     public string Name { get; set; }
     public byte[] Content { get; set; }
     public string Preview { get; set; }
+    public string PredictedClass { get; set; }
 }
